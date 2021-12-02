a092004261d2192b4b8f39b48c56f5274a0431c6
baegteun <74440939+baekteun@users.noreply.github.com>
2021-12-02 23:34:37 +0900

    Add files via upload

---
+++ TuistBasic/Project.swift
@@ -0,0 +1,22 @@
+import ProjectDescription
+
+let projectName = ""
+let organizatiionName = ""
+let bundleName = ""
+
+let project = Project(
+    name: projectName,
+    organizationName: organizatiionName,
+    targets: [
+        Target(
+            name: "\(projectName)",
+            platform: .iOS,
+            product: .app,
+            bundleId: "\(bundleName).\(projectName)",
+            deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
+            infoPlist: .file(path: Path("Support/Info.plist")),
+            sources: ["Source/**"],
+            resources: ["Resource/**"]
+        )
+    ]
+)
--- Tuist_ReactorKit_RxFlow/Source/App/SceneDelegate.swift
+++ Tuist_ReactorKit_RxFlow/Source/App/SceneDelegate.swift
@@ -1,4 +1,6 @@
 import UIKit
+import RxSwift
+import RxFlow
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
