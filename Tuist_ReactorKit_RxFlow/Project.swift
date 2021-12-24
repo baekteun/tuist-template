c98c91c8a9ff96df304a4e37d6fc3bfec959be19
baegteun <74440939+baekteun@users.noreply.github.com>
2021-12-24 22:52:38 +0900

    Add files via upload

--- Tuist_ReactorKit_RxFlow/Project.swift
+++ Tuist_ReactorKit_RxFlow/Project.swift
@@ -13,9 +13,9 @@ let project = Project(
             product: .app,
             bundleId: "\(orginazationIden).\(projectName)",
             deploymentTarget: .iOS(targetVersion: "13.0", devices: [.iphone]),
-            infoPlist: .file(path: Path("Support/Info.plist")),
-            sources: ["Source/**"],
-            resources: ["Resource/**"]
+            infoPlist: .file(path: Path("Target/Support/Info.plist")),
+            sources: ["Target/Source/**"],
+            resources: ["Target/Resource/**"]
         )
     ]
 )
